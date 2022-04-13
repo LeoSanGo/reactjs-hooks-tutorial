6c9d63680dbc453160aa2cc2040699c6ca344195
leosango <souoleo@gmail.com>
2022-04-13 11:53:48 -0300

    Second group of tests

--- src/App.test.js
+++ src/App.test.js
@@ -4,10 +4,10 @@ import AccountBalance from './components/AccountBalance';
 import Notification from './components/Notification';
 import { shallow, mount } from 'enzyme';
 
-const userBallance = {
+const userBalance = {
   balance: '1100',
   savingsBallance: '103',
-}
+};
 
 describe('rendering components', () => {
   it('renders App component without crashing', () => {
@@ -16,18 +16,42 @@ describe('rendering components', () => {
 
   it('renders App component header without crashing', () => {
     const wrapper = shallow(<App />);
-    const header = (<h1 className="has-text-centered title is-1">Welcome in the personal finance app!</h1>)
+    const header = (
+      <h1 className="has-text-centered title is-1">
+        Welcome in the personal finance app!
+      </h1>
+    );
     expect(wrapper.contains(header).toEqual(true));
-  })
+  });
 
   it('renders Notification component without crashing', () => {
     shallow(<Notification />);
   });
 
   it('renders button', () => {
-    const wrapper = mount(<AccountBalance accounts={userBallance} />);
-    const label = wrapper.find("#balance-button").text();
+    const wrapper = mount(<AccountBalance accounts={userBalance} />);
+    const label = wrapper.find('#balance-button').text();
     expect(label).toEqual('Send 100$');
   });
+});
 
+describe('passing props', () => {
+  const accountWrapper = mount(<AccountBalance accounts={userBalance} />);
+  const notificationWrapper = mount(
+    <notification balance={userBalance.balance} />
+  );
+
+  it('accepsts user account props', () => {
+    expect(accountWrapper.props().accounts).toEqual(userBalance);
+  });
+
+  it('contains savingsBalance value', () => {
+    const value = accountWrapper.find('.savings').text();
+    const expectedValue = userBalance.savingsBallance + '$';
+    expect(value).toEqual(expectedValue);
+  });
+
+  it('notification accepts props', ( => {
+    expect(notificationWrapper.props().balance).toEqual(userBalance.balance);
+  }))
 });
