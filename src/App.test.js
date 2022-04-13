f57f333ecefd24769038ae881967b9e60ecbb173
leosango <souoleo@gmail.com>
2022-04-13 13:03:06 -0300

    Tests Done

--- src/App.test.js
+++ src/App.test.js
@@ -3,10 +3,11 @@ import App from './App';
 import AccountBalance from './components/AccountBalance';
 import Notification from './components/Notification';
 import { shallow, mount } from 'enzyme';
+import toJson from 'enzyme-to-json';
 
 const userBalance = {
-  balance: '1100',
-  savingBalance: '103',
+  balance: 1100,
+  savingBalance: 103,
 };
 
 describe('rendering components', () => {
@@ -21,7 +22,7 @@ describe('rendering components', () => {
         Welcome in the personal finance app!
       </h1>
     );
-    expect(wrapper.contains(header).toEqual(true));
+    expect(wrapper.contains(header)).toEqual(true);
   });
 
   it('renders Notification component without crashing', () => {
@@ -38,7 +39,7 @@ describe('rendering components', () => {
 describe('passing props', () => {
   const accountWrapper = mount(<AccountBalance accounts={userBalance} />);
   const notificationWrapper = mount(
-    <notification balance={userBalance.balance} />
+    <Notification balance={userBalance.balance} />
   );
 
   it('accepts user account props', () => {
@@ -51,26 +52,40 @@ describe('passing props', () => {
     expect(value).toEqual(expectedValue);
   });
 
-  it('notification accepts props', ( => {
+  it('notification accepts props', () => {
     expect(notificationWrapper.props().balance).toEqual(userBalance.balance);
-  }))
+  });
 });
 
 describe("logic", () => {
   const wrapper = mount(<AccountBalance accounts={userBalance} />);
-  const notificationWrapper = mount(<Notification balance={userBalance.balance} />);
   wrapper.find('#balance-button').simulate('click');
 
   it('button click - update savings', () => {
     const savingsValue = wrapper.find('.savings').text();
-    const expectedValue = userBalance.balance + 100 + '$';
+    const expectedValue = userBalance.savingBalance + 100 + '$';
     expect(savingsValue).toEqual(expectedValue);
   });
 
   it('button click - update balance', () => {
     const balanceValue = wrapper.find('.balance').text();
-    const expectedValue = userBalance.balance - 100 + '$';
-    expect(balanceValue).toEqual(expectedValue);
+    const expectedBalanceValue = userBalance.balance - 100 + '$';
+    expect(balanceValue).toEqual(expectedBalanceValue);
   })
 })
 
+describe('snapshots', () => {
+  it('App snapshots', () => {
+    const tree = shallow( <App /> );
+    expect(toJson(tree)).toMatchSnapshot();
+  });
+  it('Accounts snapshot', () => {
+    const AccountBalanceTree = shallow(<AccountBalance accounts={userBalance} />);
+    expect(toJson(AccountBalanceTree)).toMatchSnapshot();
+  });
+
+  it('Notification snapshot', () => {
+    const notificationTree = shallow(<Notification balance={userBalance.balance} />);
+    expect(toJson(notificationTree)).toMatchSnapshot();
+  })
+})
